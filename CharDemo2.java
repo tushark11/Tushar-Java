5a8acb9c7cf7ea95a68365ad0a09f40508870651
Tushar Kapadnis <117912420+tushark11@users.noreply.github.com>
2025-02-08 15:25:22 +0530

    Add files via upload

---
+++ CharDemo2.java
@@ -0,0 +1,11 @@
+class CharDemo2{
+    public static void main(String args [])
+    {
+        char ch1;
+        ch1 = 'X';//characters can be treated as a integer that is it can be incremented like integers
+        System.out.println(ch1);
+        ch1++;//characters can be treated as a integer that is it can be incremented like integers
+        System.out.println(ch1);
+        
+    }
+}
