15623cf08f81e993cbcd0c3276ab1ebefde6496a
Tushar Kapadnis <117912420+tushark11@users.noreply.github.com>
2025-02-08 15:10:58 +0530

    Add files via upload

---
+++ AreaCircle.java
@@ -0,0 +1,12 @@
+class AreaCircle{
+    public static void main(String args[])
+
+    {
+double radius;
+double pi = 3.14;
+double area;
+radius = 20;
+area = pi*radius*radius;
+System.out.println("Area of the circle is:"+area);
+}
+}
