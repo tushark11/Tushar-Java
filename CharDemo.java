e5919d4685b8369d04ecd206f21891cf92cb9574
Tushar Kapadnis <117912420+tushark11@users.noreply.github.com>
2025-02-08 15:19:57 +0530

    Add files via upload

---
+++ CharDemo.java
@@ -0,0 +1,11 @@
+class CharDemo{
+    public static void main(String args[])
+    {
+        char ch1;
+        char ch2;
+        ch1 = 88;//it will print the character with ascii code 88 , which is X 
+        ch2 = 'Y';
+        System.out.println("ch1 and ch2:");
+        System.out.println(ch1+"  "+ch2);
+    }
+}
