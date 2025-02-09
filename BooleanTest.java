2b6d8c94246561fc784e4f1fa73c6edd377e2ccf
Tushar Kapadnis <117912420+tushark11@users.noreply.github.com>
2025-02-09 23:59:41 +0530

    Add files via upload

---
+++ BooleanTest.java
@@ -0,0 +1,11 @@
+class BooleanTest{
+    public static void main(String[]args)
+    {
+        boolean b;
+
+        b = false;
+        System.out.println("b is "+b);
+        b = true;
+        System.out.println("b is "+b);
+    }
+}
