673eeffd89c2a3ff5c563f677a66b94cdad88224
Tushar Kapadnis <117912420+tushark11@users.noreply.github.com>
2025-02-11 21:59:30 +0530

    Add files via upload

---
+++ CharLiteral.java
@@ -0,0 +1,7 @@
+class CharLiteral{
+    public static void main(String[]args)
+    {
+       char ch1=116;//it prints the character for the givrn ascii code
+        System.out.println(ch1);
+    }
+}
