49cea81680caa59940a8bab9d7109d3e2dd5c0f1
Tushar Kapadnis <117912420+tushark11@users.noreply.github.com>
2025-02-12 23:03:50 +0530

    Add files via upload

---
+++ DynamicInitialize.java
@@ -0,0 +1,10 @@
+class DynamicInitialize{
+    public static void main(String[] args)
+    {
+        int a = 5;
+        int b = 6;
+        
+        int c = a + b;
+        System.out.println("The sum of a and b is " + c);
+    }
+}
