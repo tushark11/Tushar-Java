5a4c52892ba4b7680259ac3915d38f5e1cc05eb8
Tushar Kapadnis <117912420+tushark11@users.noreply.github.com>
2025-02-13 12:23:39 +0530

    Add files via upload

---
+++ OneD_Array.java
@@ -0,0 +1,18 @@
+class OneD_Array
+{
+    public static void main(String[] args)    
+    {
+       
+       int[] x = new int[5];
+         x[0] = 10;
+            x[1] = 20;
+            x[2] = 30;
+            x[3] = 40;
+            x[4] = 50;
+            for(int i = 0; i < 5; i++)
+            {
+                System.out.println(x[i]);
+            }
+       
+        }
+    }
