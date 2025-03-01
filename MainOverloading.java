9eab035e1514c7c61ca6eb9cd210f8d860dbc05f
Tushar Kapadnis <117912420+tushark11@users.noreply.github.com>
2025-03-02 00:02:11 +0530

    Add files via upload

---
+++ MainOverloading.java
@@ -0,0 +1,11 @@
+class MainOverloading
+{
+    public static void main(String[] args)
+    {
+        System.out.println("String[] ");
+    }
+    public static void main(String args)
+    {
+        System.out.printl("main(String args)");
+    }
+}
