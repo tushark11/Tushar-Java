148a2b2ee20ea068391bd64fc090988b8737c50b
Tushar Kapadnis <117912420+tushark11@users.noreply.github.com>
2025-03-02 00:17:39 +0530

    Add files via upload

---
+++ MainOverriding.java
@@ -0,0 +1,14 @@
+class MainOverriding
+{
+    public static void main(String[] args)
+    {
+        System.out.println("parent main");
+    }
+}
+class Child
+{
+    public static void main(String[] args)
+    {
+        System.out.println("child main");
+    }
+}
