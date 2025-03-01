3776d09a893a1494dd4a4efb0e8f7b82347001d9
Tushar Kapadnis <117912420+tushark11@users.noreply.github.com>
2025-03-02 00:14:36 +0530

    Add files via upload

---
+++ MainInheritance.java
@@ -0,0 +1,12 @@
+class MainInheritance{
+  
+    {
+        System.out.println("PARENT MAIN");
+    }
+}
+class child extends MainInheritance{
+    public static void main(String[] args)
+    {
+        System.out.println("CHILD MAIN");
+    }
+}
